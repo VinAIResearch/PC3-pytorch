50d4864c9c91c78edfae195ea46869faaa131736
tungnd1705 <ductungnguyen1997@gmail.com>
2020-01-16 16:56:08 +0700

    calculate the min variance of latent dynamics

--- map_scale_stat.py
+++ map_scale_stat.py
@@ -28,14 +28,17 @@ def calc_avg_dyn_std(model, env_name, sample_size=5000, noise=0):
     dataset = dataset(sample_size=sample_size, noise=noise)
     data_loader = DataLoader(dataset, batch_size=100, shuffle=False, drop_last=False, num_workers=1)
 
-    avg_std = 0.0
+    all_std = None
     for x, u, x_next in data_loader:
         with torch.no_grad():
             z = model.encode(x)
             z_next_trans_dist, _, _ = model.transition(z, u)
             stddev = z_next_trans_dist.stddev.numpy()
-            avg_std += np.mean(stddev, axis=0)
-    return avg_std / len(data_loader)
+            if all_std is None:
+                all_std = stddev
+            else:
+                all_std = np.concatenate((all_std, stddev), axis=0)
+    return np.mean(all_std, axis=0), np.min(all_std, axis=0)
 
 def main(args):
     env_name = args.env
@@ -62,23 +65,25 @@ def main(args):
         model.eval()
 
         avg_norm_2 = calc_avg_norm_2(model, env_name)
-        avg_std = calc_avg_dyn_std(model, env_name)
+        avg_std, min_std = calc_avg_dyn_std(model, env_name)
+        # print ('avg std:' + str(avg_std))
+        print ('min std: ' + str(min_std))
         all_avg_norm_2.append(avg_norm_2)
         all_avg_dyn_std.append(avg_std)
 
-    # compute mean and variance
-    all_avg_norm_2 = np.array(all_avg_norm_2)
-    mean_norm_2 = np.mean(all_avg_norm_2)
-    var_norm_2 = np.var(all_avg_norm_2)
-
-    all_avg_dyn_std = np.array(all_avg_dyn_std)
-    mean_dyn_std = np.mean(all_avg_dyn_std, axis=0)
-    var_dyn_std = np.var(all_avg_dyn_std, axis=0)
-
-    print ('Mean of average norm 2: ' + str(mean_norm_2))
-    print ('Variance of average norm 2: ' + str(var_norm_2))
-    print('Mean of average dynamic std: ' + str(mean_dyn_std))
-    print('Variance of average dynamic std: ' + str(var_dyn_std))
+    # # compute mean and variance
+    # all_avg_norm_2 = np.array(all_avg_norm_2)
+    # mean_norm_2 = np.mean(all_avg_norm_2)
+    # var_norm_2 = np.var(all_avg_norm_2)
+    #
+    # all_avg_dyn_std = np.array(all_avg_dyn_std)
+    # mean_dyn_std = np.mean(all_avg_dyn_std, axis=0)
+    # var_dyn_std = np.var(all_avg_dyn_std, axis=0)
+    #
+    # print ('Mean of average norm 2: ' + str(mean_norm_2))
+    # print ('Variance of average norm 2: ' + str(var_norm_2))
+    # print('Mean of average dynamic std: ' + str(mean_dyn_std))
+    # print('Variance of average dynamic std: ' + str(var_dyn_std))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='compute latent map scale statistics')
