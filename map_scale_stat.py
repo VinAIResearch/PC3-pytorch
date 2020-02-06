a080776527acf87d0f0e64320a5df11d8c44a527
Tung <tungnd13@sdc2-hanoi.vinai.io-pp>
2020-02-06 13:07:05 +0700

    compute the latent map scale of F \circ E

--- map_scale_stat.py
+++ map_scale_stat.py
@@ -7,21 +7,28 @@ from torch.utils.data import DataLoader
 
 from pcc_model import PCC
 from datasets import PlanarDataset, PendulumDataset, CartPoleDataset
+from mdp.plane_obstacles_mdp import PlanarObstaclesMDP
+from mdp.pendulum_mdp import PendulumMDP
 
 env_data_dim = {'planar': (1600, 2, 2), 'pendulum': ((2,48,48), 3, 1), 'cartpole': ((2,80,80), 8, 1)}
 datasets = {'planar': PlanarDataset, 'pendulum': PendulumDataset, 'cartpole': CartPoleDataset}
 
-def calc_avg_norm_2(model, env_name, sample_size=5000, noise=0):
+def calc_scale(model, mdp, env_name, sample_size=5000, noise=0):
     dataset = datasets[env_name]
     dataset = dataset(sample_size=sample_size, noise=noise)
     data_loader = DataLoader(dataset, batch_size=100, shuffle=False, drop_last=False, num_workers=1)
 
     avg_norm_2 = 0.0
+    avg_dynamics_norm_2 = 0.0
     for x, _, _ in data_loader:
         with torch.no_grad():
             z = model.encode(x)
+            u = [mdp.sample_random_action() for _ in range(100)]
+            u = torch.Tensor(u)
+            z_next = model.transition(z, u)[0].mean
             avg_norm_2 += torch.mean(torch.sum(z.pow(2), dim=1))
-    return avg_norm_2 / len(data_loader)
+            avg_dynamics_norm_2 += torch.mean(torch.sum(z_next.pow(2), dim=1))
+    return avg_norm_2 / len(data_loader), avg_dynamics_norm_2 / len(data_loader)
 
 def calc_avg_dyn_std(model, env_name, sample_size=5000, noise=0):
     dataset = datasets[env_name]
@@ -42,39 +49,46 @@ def calc_avg_dyn_std(model, env_name, sample_size=5000, noise=0):
 
 def main(args):
     env_name = args.env
-    assert env_name in ['planar', 'pendulum', 'pendulum_gym', 'cartpole', 'mountain_car']
+    assert env_name in ['planar', 'pendulum', 'cartpole']
+    if env_name == 'planar':
+        mdp = PlanarObstaclesMDP()
+    elif env_name == 'pendulum':
+        mdp = PendulumMDP()
     setting_path = args.setting_path
     epoch = args.epoch
 
     x_dim, z_dim, u_dim = env_data_dim[env_name]
-    if env_name in ['planar', 'pendulum', 'pendulum_gym', 'mountain_car']:
+    if env_name in ['planar', 'pendulum']:
         x_dim = np.prod(x_dim)
 
     all_avg_norm_2 = []
+    all_avg_dyn_norm_2 = []
     all_avg_dyn_std = []
     log_folders = [os.path.join(setting_path, dI) for dI in os.listdir(setting_path)
                    if os.path.isdir(os.path.join(setting_path, dI))]
     for log in log_folders:
         with open(log + '/settings', 'r') as f:
             settings = json.load(f)
-            armotized = settings['armotized']
+            # armotized = settings['armotized']
 
         # load the trained model
-        model = PCC(armotized, x_dim, z_dim, u_dim, env_name)
+        model = PCC(False, x_dim, z_dim, u_dim, env_name)
         model.load_state_dict(torch.load(log + '/model_' + str(epoch), map_location='cpu'))
         model.eval()
 
-        avg_norm_2 = calc_avg_norm_2(model, env_name)
+        avg_norm_2, avg_dyn_norm_2 = calc_scale(model, mdp, env_name)
         avg_std, min_std = calc_avg_dyn_std(model, env_name)
-        # print ('avg std:' + str(avg_std))
-        # print ('min std: ' + str(min_std))
         all_avg_norm_2.append(avg_norm_2)
+        all_avg_dyn_norm_2.append(avg_dyn_norm_2)
         all_avg_dyn_std.append(avg_std)
 
     # compute mean and variance
     all_avg_norm_2 = np.array(all_avg_norm_2)
     mean_norm_2 = np.mean(all_avg_norm_2)
     var_norm_2 = np.var(all_avg_norm_2)
+    all_avg_dyn_norm_2 = np.array(all_avg_dyn_norm_2)
+    mean_dyn_norm_2 = np.mean(all_avg_dyn_norm_2)
+    var_dyn_norm_2 = np.var(all_avg_dyn_norm_2)
     
     all_avg_dyn_std = np.array(all_avg_dyn_std)
     mean_dyn_std = np.mean(all_avg_dyn_std, axis=0)
@@ -82,6 +96,8 @@ def main(args):
     
     print ('Mean of average norm 2: ' + str(mean_norm_2))
     print ('Variance of average norm 2: ' + str(var_norm_2))
+    print ('Mean of average dynamics norm 2: ' + str(mean_dyn_norm_2))
+    print ('Variance of average dynamics norm 2: ' + str(var_dyn_norm_2))
     print('Mean of average dynamic std: ' + str(mean_dyn_std))
     print('Variance of average dynamic std: ' + str(var_dyn_std))
 
